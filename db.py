a2a8fb2c7fd91682338ccbf7f539acf2b4d0fcca
Abdul Cherchar <chercharabdul@gmail.com>
2024-04-30 18:47:32 -0400

    added date class

--- db.py
+++ db.py
@@ -23,3 +23,41 @@ class User(db.Model):
     return {
       "current courses": [c.serialize() for c in self.current_courses]
     }  
+
+
+course_day_association = db.Table('course_days',
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True),
+    db.Column('day_id', db.Integer, db.ForeignKey('day.id'), primary_key=True)
+)
+
+
+
+class Day(db.Model):
+    """
+    Day Model for scheduling purposes
+    """
+
+    __tablename__ = "day"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)  
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))  
+    courses = db.relationship('Course', secondary='course_day_association', back_populates='days')
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get("name", "")
+
+    def serialize(self):
+        """
+        Serialize a day object, potentially including time slots
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+
+
+
+
+
